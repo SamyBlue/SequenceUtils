907a8d0faed3f942cf063df8d1637b98121c4dbb
Samy <33347703+SamyBlue@users.noreply.github.com>
2021-11-16 17:04:16 +0000

    Add special case handler when #keypoints = 2

--- NumSeqMap.lua
+++ NumSeqMap.lua
@@ -4,24 +4,28 @@ NumSeqMap.__index = NumSeqMap
 
 -- Stores a NumberSequence as a mapping array for efficient O(1) reads
 -- specifying mapSize is optional and represents the number of equally-spaced samples taken to accurately represent the number sequence in map format
-function NumSeqMap.new(numSeq, mapSize) --TODO: Change to maxMapSize where mapSize adapts to number of keypoints? Special case when #keypoints = 2
+function NumSeqMap.new(numSeq, mapSize)
 	local self = setmetatable({}, NumSeqMap)
 	self._mapSize = mapSize or 20 -- must be an integer
 	self._map = {}
 	
-	local SAMPLING_INTERVAL = 1 / self._mapSize
 	local keypoints = numSeq.Keypoints
-	
+
+	if #keypoints == 2 then -- Special case where can construct map with 100% accuracy
+		self._mapSize = 1
+	end
+
 	self._map[0] = keypoints[1].Value
 	self._map[self._mapSize] = keypoints[#keypoints].Value
-	
+
+	self._samplingInterval = 1 / self._mapSize
 	local kpointIndex = 2
 	
 	for i = 1, self._mapSize - 1 do
 		
-		local kpointTime = keypoints[kpointIndex].Time
+		-- local kpointTime = keypoints[kpointIndex].Time
 		
-		local alpha = i * SAMPLING_INTERVAL
+		-- local alpha = i * SAMPLING_INTERVAL
 		
 	end
 
