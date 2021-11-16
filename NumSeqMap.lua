3598338df3567b409d32885ebf5186c7ed3b8831
Samy <33347703+SamyBlue@users.noreply.github.com>
2021-11-16 15:43:22 +0000

    Constructor Defined

--- NumSeqMap.lua
+++ NumSeqMap.lua
@@ -1,3 +1,33 @@
 local NumSeqMap = {} -- Define new class
 NumSeqMap.__type = "NumSeqMap"
 NumSeqMap.__index = NumSeqMap
+
+-- Stores a NumberSequence as a mapping array for efficient O(1) reads
+-- specifying mapSize is optional and represents the number of equally-spaced samples taken to accurately represent the number sequence in map format
+function NumSeqMap.new(numSeq, mapSize) --TODO: Change to maxMapSize where mapSize adapts to number of keypoints? Special case when #keypoints = 2
+	local self = setmetatable({}, NumSeqMap)
+	self._mapSize = mapSize or 60 -- must be an integer
+	self._map = {}
+	
+	local SAMPLING_INTERVAL = 1 / self._mapSize
+	local keypoints = numSeq.Keypoints
+	
+	self._map[0] = keypoints[1].Value
+	self._map[self._mapSize] = keypoints[#keypoints].Value
+	
+	local kpointIndex = 2
+	
+	for i = 1, self._mapSize - 1 do
+		
+		local kpointTime = keypoints[kpointIndex].Time
+		
+		local alpha = i * SAMPLING_INTERVAL
+		
+	end
+
+	return self
+end
+
+
+
+return NumSeqMap
