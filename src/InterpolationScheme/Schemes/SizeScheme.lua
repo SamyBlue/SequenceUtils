587091826ee0a142f0bd1c6cb6d8767594a29d32
Samy <33347703+SamyBlue@users.noreply.github.com>
2021-12-07 17:00:31 +0000

    Add comments explaining attributes for SizeScheme

--- src/InterpolationScheme/Schemes/SizeScheme.lua
+++ src/InterpolationScheme/Schemes/SizeScheme.lua
@@ -4,14 +4,14 @@ local NumSeqMap = require(script.Parent.Parent.Parent.SequenceMaps.NumSeqMap)
 local SizeScheme = {}
 
 SizeScheme.Attributes = {
-    ["SizeFrom"] = Vector3.new(0, 0, 0),
-    ["SizeMin"] = Vector3.new(1, 1, 1),
-    ["SizeMax"] = Vector3.new(1, 1, 1),
+    ["SizeFrom"] = Vector3.new(0, 0, 0), --* Coordinates must be specified in initial object space of relevant instance
+    ["SizeMin"] = Vector3.new(1, 1, 1), --! All x, y, z Vector3 components assumed non-zero
+    ["SizeMax"] = Vector3.new(1, 1, 1), --! All x, y, z Vector3 components assumed non-zero
     ["SizeSequence"] = NumberSequence.new(1)
 }
 
 SizeScheme.LoopCondition = function (instance)
-    return IsSequenceNotConstant(#instance:GetAttribute("SizeSequence"))
+    return IsSequenceNotConstant(instance:GetAttribute("SizeSequence"))
 end
 
 
