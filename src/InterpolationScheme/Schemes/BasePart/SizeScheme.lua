a46cf9f8983d258a3d61befd69469f652f195c1f
Samy <33347703+SamyBlue@users.noreply.github.com>
2021-12-09 06:45:36 +0000

    Replace Min with Start and Max with Goal

--- src/InterpolationScheme/Schemes/BasePart/SizeScheme.lua
+++ src/InterpolationScheme/Schemes/BasePart/SizeScheme.lua
@@ -5,8 +5,8 @@ local SizeScheme = {}
 
 SizeScheme.Attributes = {
     ["SizeFrom"] = Vector3.new(0, 0, 0), --* Coordinates must be specified in initial object space of relevant instance
-    ["SizeMin"] = Vector3.new(1, 1, 1), --! All x, y, z Vector3 components assumed non-zero
-    ["SizeMax"] = Vector3.new(1, 1, 1), --! All x, y, z Vector3 components assumed non-zero
+    ["SizeStart"] = Vector3.new(1, 1, 1), --! All x, y, z Vector3 components assumed non-zero
+    ["SizeGoal"] = Vector3.new(1, 1, 1), --! All x, y, z Vector3 components assumed non-zero
     ["SizeSequence"] = NumberSequence.new(1)
 }
 
@@ -31,23 +31,23 @@ SizeScheme.Play = function (instance, timeLength, applyTo)
 
     local SizeFrom = instance:GetAttribute("SizeFrom")
     
-    local Min, Max = instance:GetAttribute("SizeMin"), instance:GetAttribute("SizeMax")
-    local Diff = Max - Min
+    local Start, Goal = instance:GetAttribute("SizeStart"), instance:GetAttribute("SizeGoal")
+    local Diff = Goal - Start
     local SeqMap = NumSeqMap.new(instance:GetAttribute("SizeSequence"))
 
     if SizeFrom == Vector3.new(0, 0, 0) then
         HeartbeatLoopFor(timeLength, function (_, _, interp)
-            applyTo.Size = Min + Diff * SeqMap:GetValue(interp)
+            applyTo.Size = Start + Diff * SeqMap:GetValue(interp)
         end, function ()
-            applyTo.Size = Max
+            applyTo.Size = Goal
         end)
     else        
         HeartbeatLoopFor(timeLength, function (_, _, interp)
-            local scale = Min + Diff * SeqMap:GetValue(interp)
+            local scale = Start + Diff * SeqMap:GetValue(interp)
             applyTo.Position = applyTo.CFrame:PointToWorldSpace(SizeFrom - SizeFrom * scale)
             applyTo.Size = scale
         end, function ()
-            applyTo.Size = Max
+            applyTo.Size = Goal
         end)
     end
 
