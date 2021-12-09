d74d8d474ba8eb7e907c6847ae9d0f02955ae5bd
Samy <33347703+SamyBlue@users.noreply.github.com>
2021-12-09 07:29:46 +0000

    Add TransparencyScheme

---
+++ src/InterpolationScheme/Schemes/BasePart/TransparencyScheme.lua
@@ -0,0 +1,42 @@
+local HeartbeatLoopFor = require(script.Parent.Parent.Parent.Parent.Loops.HeartbeatLoopFor)
+local IsSequenceNotConstant = require(script.Parent.Parent.Parent.SchemesUtil.IsSequenceNotConstant)
+local NumSeqMap = require(script.Parent.Parent.Parent.Parent.SequenceMaps.NumSeqMap)
+local TransparencyScheme = {}
+
+TransparencyScheme.Attributes = {
+    ["TransparencyGoal"] = 1,
+    ["TransparencySequence"] = NumberSequence.new(0)
+}
+
+TransparencyScheme._LoopCondition = function (instance)
+    return IsSequenceNotConstant(instance:GetAttribute("TransparencySequence"))
+end
+
+TransparencyScheme.Setup = function (instance)
+    --Setup Attributes
+    for attribute, value in ipairs(TransparencyScheme.Attributes) do
+        instance:SetAttribute(attribute, value)
+    end
+end
+
+TransparencyScheme.Play = function (instance, timeLength, applyTo)
+    
+    if not TransparencyScheme._LoopCondition(instance) then
+        return
+    end
+
+    applyTo = applyTo or instance -- (Optional) Specify an alternative instance to apply scheme to
+    
+    local Start, Goal = applyTo.Transparency, instance:GetAttribute("TransparencyGoal")
+    local Diff = Goal - Start
+    local SeqMap = NumSeqMap.new(instance:GetAttribute("TransparencySequence"))
+
+    HeartbeatLoopFor(timeLength, function (_, _, interp)
+        applyTo.Transparency = Start + Diff * SeqMap:GetValue(interp)
+    end, function ()
+        applyTo.Transparency = Goal
+    end)
+
+end
+
+return TransparencyScheme
